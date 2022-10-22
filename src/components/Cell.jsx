67ab09c242523e91acae729fdfe5a13407ab0d5a
NilsGke <n.j.goeke@gmail.com>
2022-10-22 09:57:57 +0200

    added edit buttons

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,14 +1,58 @@
-const Cell = ({ active, content, change }) => {
+import { useEffect, useState } from "react";
+import { AiFillDelete, AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+const Cell = ({
+    active,
+    data,
+    change,
+    editMode,
+    delete: deleteFun,
+    canMove,
+    move,
+}) => {
+    const [letter, setLetter] = useState(data.letter);
+
+    useEffect(() => {
+        if (letter !== data.letter) change({ ...data, letter });
+    }, [letter]);
+
     return (
         <div className={"cell" + (active ? " active" : "")}>
+            {editMode ? (
+                <div className="move">
+                    {canMove.left ? (
+                        <button className="move left" onClick={() => move(-1)}>
+                            <AiOutlineLeft />
+                        </button>
+                    ) : (
+                        ""
+                    )}
+                    {canMove.right ? (
+                        <button className="move right" onClick={() => move(1)}>
+                            <AiOutlineRight />
+                        </button>
+                    ) : (
+                        ""
+                    )}
+                </div>
+            ) : (
+                ""
+            )}
             <input
+                id={"stripInput" + data.id}
                 type="text"
-                value={content}
-                onChange={(e) => change(e.target.value)}
+                value={letter}
+                onChange={(e) => setLetter(e.target.value)}
                 maxLength={1}
             />
+            {editMode ? (
+                <button className="delete" onClick={deleteFun}>
+                    <AiFillDelete />
+                </button>
+            ) : (
+                ""
+            )}
         </div>
     );
 };
-
 export default Cell;
